d3ab349e87a3f3791fdce01967ad41274d938fba
Mohit Kumar <mohit.kumar1@in.abb.com>
2022-06-10 15:56:55 +0530

    protect server with authentication service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { EditServerComponent } from "./servers/edit-server/edit-server.component
 import { ServerComponent } from "./servers/server/server.component";
 import { UserComponent } from "./users/user/user.component";
 import { RouterModule } from '@angular/router';
+import { AuthService } from "./auth.service";
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent },
@@ -14,7 +15,7 @@ const appRoutes: Routes = [
       { path: 'users/:id/:name', component: UserComponent }  
     ] 
     },
-    { path: 'servers', component: ServerComponent, children: 
+    { path: 'servers', canActivate: [AuthService],component: ServerComponent, children: 
     [
       { path: ':id/edit', component: EditServerComponent },
       { path: ':id/', component: ServerComponent } 
