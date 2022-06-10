04135d0f7614922c1888b623f5df32fa98a6dff4
Mohit Kumar <mohit.kumar1@in.abb.com>
2022-06-10 15:31:50 +0530

    creating separate routing module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from "@angular/core";
+import { Routes } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { EditServerComponent } from "./servers/edit-server/edit-server.component";
+import { ServerComponent } from "./servers/server/server.component";
+import { UserComponent } from "./users/user/user.component";
+import { RouterModule } from '@angular/router';
+
+const appRoutes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'users', component: UserComponent, children: 
+    [
+      { path: 'users/:id/:name', component: UserComponent }  
+    ] 
+    },
+    { path: 'servers', component: ServerComponent, children: 
+    [
+      { path: ':id/edit', component: EditServerComponent },
+      { path: ':id/', component: ServerComponent } 
+    ] 
+    },
+    { path: 'not-found', component: PageNotFoundComponent},
+    { path: 'something', redirectTo: '/not-found'}
+    
+  ];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(appRoutes)
+    ],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+}
